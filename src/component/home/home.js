387178eb92afe7bafca57745596bd596b08009ac
sanket9 <sanket.mukherjee@gmail.com>
2018-08-16 00:44:20 +0530

    loader added

--- src/component/home/home.js
+++ src/component/home/home.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Col, Button, Form, FormGroup, Label, Input, Row, Card } from 'reactstrap';
 import './home.css';
 import ScriptTag from 'react-script-tag';
-
+import loader from "../../assets/loader.gif"
 import Headers from '../header/header'
 
 class Home extends Component {
@@ -32,19 +32,24 @@ class Home extends Component {
         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${res[0]}&appid=${APP_KEY}`)
         .then(resp=> resp.json()).then(data=>{
             console.log(data);
-            let orgTemp = parseInt(data.main.temp) - 273.15;
-            this.setState({
-                value: input,
-                loading: false,
-                temp: Math.round(orgTemp),
-                humidity: data.main.humidity,
-                name: data.name,
-                country: data.sys.country,
-                weather: data.weather[0].main,
-                weather_icon: data.weather[0].icon,
+            if(data.cod !== 200){
+                alert(data.message);
+            }else{
+                let orgTemp = parseInt(data.main.temp) - 273.15;
+                this.setState({
+                    value: input,
+                    loading: false,
+                    temp: Math.round(orgTemp),
+                    humidity: data.main.humidity,
+                    name: data.name,
+                    country: data.sys.country,
+                    weather: data.weather[0].main,
+                    weather_icon: data.weather[0].icon,
 
-            })
-            console.log(this.state);
+                })
+                console.log(this.state);
+            }
+            
             
         },err=>{
             console.log(err);            
@@ -70,13 +75,18 @@ class Home extends Component {
                             </FormGroup>
                             <Button onClick={this.getWeather}>Get Weather</Button>
                         </Form> 
-                        {this.state.temp && this.state.weather && <p className="text-center">
-                            <img className="img-weather" src={`http://openweathermap.org/img/w/${this.state.weather_icon}.png`}/>
-                            <span className="temp">{this.state.temp}° <small className="temp-in">C </small></span>
-                        </p>}
-                        {this.state.name && this.state.country &&<p className="text-center">
-                            <span className="country-name">{this.state.name}, {this.state.country}</span>
-                        </p>}
+                        
+                        {this.state.loading == true ? <img className="loader" src={loader} />: ''}
+
+                        {this.state.name && <div>
+                            {this.state.temp && this.state.weather && <p className="text-center">
+                                <img className="img-weather" src={`http://openweathermap.org/img/w/${this.state.weather_icon}.png`}/>
+                                <span className="temp">{this.state.temp}° <small className="temp-in">C </small></span>
+                            </p>}
+                            {this.state.name && this.state.country &&<p className="text-center">
+                                <span className="country-name">{this.state.name}, {this.state.country}</span>
+                            </p>}
+                        </div>}
                         
                     </Col>            
                 
